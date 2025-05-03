82f30535a35b409e07358fe096ec74c596bda920
Guilherme Augusto <guilhermedev23@gmail.com>
2025-05-02 21:34:42 -0300

    ok

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,40 @@
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
+import { Participant } from '../../components/Participant';
+
 
 export function Home() {
 
-    function handleParticipantAdd(){
-      console.log("voce Clicou no Botão De Adicionar!");
-    }
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar!");
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
         Nome Do Evento
       </Text>
+
       <Text style={styles.eventDate}>
-        Sexta , 4 De Novembro De 2025.
+        Sexta, 4 de Novembro de 2025.
       </Text>
-       <View style={styles.form}>
-      <TextInput style={styles.input}
-        placeholder="Nome Do Participante"
-        placeholderTextColor="#6B6B6B"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>
-           +
-        </Text>
-      </TouchableOpacity>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
-  )
+  );
 }
